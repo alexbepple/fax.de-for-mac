0e75593abe327480beb4431d2989ec62dfd82bcc
Alex Bepple <alex@bepple.de>
2011-06-25 00:18:35 +0200

    Default: preview + send; option to only preview

--- send.rb
+++ send.rb
@@ -4,9 +4,23 @@ require 'rubygems'
 require 'savon'
 require 'base64'
 require 'yaml'
+require 'optparse'
 
 prefs = YAML.load_file('prefs.yml')
 settings = YAML.load_file('settings.yml')
+
+options = {}
+options_parser = OptionParser.new do |opts|
+    options[:dry_run] = false
+    opts.on('-n', '--dry-run', 'Create a preview, but do not actually send the letter.') do
+        options[:dry_run] = true
+    end
+    opts.on('-h', '--help', 'Guess what.') do 
+        puts opts
+        exit
+    end
+end
+options_parser.parse!
 
 HTTPI.log = false
 Savon.configure do |config|
@@ -37,9 +51,24 @@ end
 
 result = response.to_hash[:send_response]
 return_code = result[:return]
-p return_code
+abort(return_code) if return_code != "0"
 
-if return_code == "0"
-    doc = result[:check_doc_file]
-    File.open('result.tiff', 'wb') { |f| f.write(Base64.decode64(doc)) }
+doc = result[:check_doc_file]
+File.open('result.tiff', 'wb') { |f| f.write(Base64.decode64(doc)) }
+exit if options[:dry_run]
+
+response = client.request :send do
+    bw_iletter = 3
+    soap.body = {
+        :account => prefs['account'], 
+        :password => prefs['password'],
+        :job_art => bw_iletter,
+        :empfaenger_nr => name_of_file_to_send,
+        :send_filename1 => name_of_file_to_send,
+        :send_file1 => encoded_contents
+    }
 end
+result = response.to_hash[:send_response]
+return_code = result[:return]
+abort(return_code) if return_code != "0"
+
