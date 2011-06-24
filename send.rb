63493579ab905f2649e04b031adc1165bc31ce50
Alex Bepple <alex@bepple.de>
2011-06-25 01:44:51 +0200

    Encapsulated account and letter

--- send.rb
+++ send.rb
@@ -19,29 +19,57 @@ options_parser = OptionParser.new do |opts|
 end
 options_parser.parse!
 
-HTTPI.log = false
-Savon.configure do |config|
-    config.log = false
+class Account
+    attr :account, :password
+
+    def initialize(account, password)
+        @account = account
+        @password = password
+    end
+
+    def to_hash
+        {:account => @account, :password => @password}
+    end
 end
 
-name_of_file_to_send = ARGV[0]
-path_to_file = File.join(Dir.pwd, name_of_file_to_send)
-contents = File.open(path_to_file, 'rb') { |f| f.read }
-encoded_contents = Base64.encode64(contents)
+class FaxDeLetter
+    def initialize(fax_de, args={})
+        @fax_de = fax_de
+        @account = args[:account]
+        @name = args[:name]
+        @contents = Base64.encode64(args[:contents])
+    end
+    
+    def generate_preview
+        @fax_de.send to_hash.merge({:schalter => 'TEST'}) do |result|
+            doc = result[:check_doc_file]
+            yield Base64.decode64(doc)
+        end
+    end
 
-prefs = YAML.load_file('prefs.yml')
-bw_iletter = 3
-default_send_parameters = {
-    :account => prefs['account'], 
-    :password => prefs['password'],
-    :job_art => bw_iletter,
-    :empfaenger_nr => name_of_file_to_send,
-    :send_filename1 => name_of_file_to_send,
-    :send_file1 => encoded_contents
-}
+    def send
+        @fax_de.send to_hash
+    end
+
+    def to_hash
+        prefs = YAML.load_file('prefs.yml')
+        bw_iletter = 3
+        @account.to_hash.merge({
+            :job_art => bw_iletter,
+            :empfaenger_nr => @name,
+            :send_filename1 => @name,
+            :send_file1 => @contents
+        })
+    end
+end
 
 class FaxDeService
     def initialize(wsdl_url)
+        HTTPI.log = false
+        Savon.configure do |config|
+            config.log = false
+        end
+
         @client = Savon::Client.new do
             wsdl.document = wsdl_url
         end
@@ -61,11 +89,18 @@ end
 
 settings = YAML.load_file('settings.yml')
 fax_de = FaxDeService.new(settings['wsdl'])
-fax_de.send default_send_parameters.merge({:schalter => 'TEST'}) do |result|
-    doc = result[:check_doc_file]
-    File.open('result.tiff', 'wb') { |f| f.write(Base64.decode64(doc)) }
+
+prefs = YAML.load_file('prefs.yml')
+account = Account.new prefs['account'], prefs['password']
+
+name_of_file_to_send = ARGV[0]
+path_to_file = File.join(Dir.pwd, name_of_file_to_send)
+contents = File.open(path_to_file, 'rb') { |f| f.read }
+
+letter = FaxDeLetter.new(fax_de, :account => account, :name => name_of_file_to_send, :contents => contents)
+
+letter.generate_preview do |preview|
+    File.open('result.tiff', 'wb') { |f| f.write(preview) }
 end
-
 exit if options[:dry_run]
-
-fax_de.send default_send_parameters
+letter.send
