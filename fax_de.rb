db3f63e3ac1af7b076984413712ab05860639185
Alex Bepple <alex@bepple.de>
2011-06-25 02:13:52 +0200

    Extracted class definitions into own file

---
+++ fax_de.rb
@@ -0,0 +1,72 @@
+require 'rubygems'
+require 'savon'
+
+class Account
+    attr :account, :password
+
+    def initialize(account, password)
+        @account = account
+        @password = password
+    end
+
+    def to_hash
+        {:account => @account, :password => @password}
+    end
+end
+
+class FaxDeLetter
+    def initialize(fax_de, name, contents)
+        @fax_de = fax_de
+        @name = name
+        @contents = Base64.encode64(contents)
+    end
+    
+    def generate_preview
+        @fax_de.send to_hash.merge({:schalter => 'TEST'}) do |result|
+            doc = result[:check_doc_file]
+            yield Base64.decode64(doc)
+        end
+    end
+
+    def send
+        @fax_de.send to_hash
+    end
+
+    def to_hash
+        bw_iletter = 3
+        {
+            :job_art => bw_iletter,
+            :empfaenger_nr => @name,
+            :send_filename1 => @name,
+            :send_file1 => @contents
+        }
+    end
+end
+
+class FaxDeService
+    def initialize(wsdl_url, account)
+        HTTPI.log = false
+        Savon.configure do |config|
+            config.log = false
+        end
+
+        @client = Savon::Client.new do
+            wsdl.document = wsdl_url
+        end
+
+        @account = account
+    end
+
+    def send(parameters)
+        account = @account
+        response = @client.request :send do
+            soap.body = account.to_hash.merge(parameters)
+        end
+        result = response.to_hash[:send_response]
+        return_code = result[:return]
+        abort(return_code) if return_code != "0"
+
+        yield result
+    end
+end
+
--- send.rb
+++ send.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
-require 'savon'
 require 'base64'
 require 'yaml'
 require 'optparse'
+
+require 'fax_de'
 
 options = {}
 options_parser = OptionParser.new do |opts|
@@ -18,75 +18,6 @@ options_parser = OptionParser.new do |opts|
     end
 end
 options_parser.parse!
-
-class Account
-    attr :account, :password
-
-    def initialize(account, password)
-        @account = account
-        @password = password
-    end
-
-    def to_hash
-        {:account => @account, :password => @password}
-    end
-end
-
-class FaxDeLetter
-    def initialize(fax_de, name, contents)
-        @fax_de = fax_de
-        @name = name
-        @contents = Base64.encode64(contents)
-    end
-    
-    def generate_preview
-        @fax_de.send to_hash.merge({:schalter => 'TEST'}) do |result|
-            doc = result[:check_doc_file]
-            yield Base64.decode64(doc)
-        end
-    end
-
-    def send
-        @fax_de.send to_hash
-    end
-
-    def to_hash
-        bw_iletter = 3
-        {
-            :job_art => bw_iletter,
-            :empfaenger_nr => @name,
-            :send_filename1 => @name,
-            :send_file1 => @contents
-        }
-    end
-end
-
-class FaxDeService
-    def initialize(wsdl_url, account)
-        HTTPI.log = false
-        Savon.configure do |config|
-            config.log = false
-        end
-
-        @client = Savon::Client.new do
-            wsdl.document = wsdl_url
-        end
-
-        @account = account
-    end
-
-    def send(parameters)
-        account = @account
-        response = @client.request :send do
-            soap.body = account.to_hash.merge(parameters)
-        end
-        result = response.to_hash[:send_response]
-        return_code = result[:return]
-        abort(return_code) if return_code != "0"
-
-        yield result
-    end
-end
 
 account_prefs = YAML.load_file('account.yml')
 account = Account.new account_prefs['account'], account_prefs['password']
