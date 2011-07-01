bedc5a8afc9e0fbf27bd5359e6c8f69d52aecf6b
Alex Bepple <alex@bepple.de>
2011-07-02 01:02:26 +0200

    Moved preferences and settings to ~/.fax.de

--- send.rb
+++ send.rb
@@ -1,14 +1,6 @@
 #!/usr/bin/env ruby
 
-require 'pathname'
-dir = File.dirname(Pathname.new(__FILE__).realpath)
-
-require 'yaml'
 require 'optparse'
-
-require 'fax_de'
-require 'fax_de_growl'
-
 options = {}
 options_parser = OptionParser.new do |opts|
     options[:dry_run] = false
@@ -23,12 +15,20 @@ end
 options_parser.parse!
 
 
+require 'yaml'
+def load_yaml(yaml)
+    YAML.load_file(File.expand_path("~/.fax.de/#{yaml}.yml"))
+end
+
+account_prefs = load_yaml('account')
+settings = load_yaml('settings')
+
+
+require 'fax_de_growl'
 notifier = FaxDeGrowlNotifier.new
 
-account_prefs = YAML.load_file(File.join(dir, 'account.yml'))
+require 'fax_de'
 account = Account.new account_prefs['account'], account_prefs['password']
-
-settings = YAML.load_file(File.join(dir, 'settings.yml'))
 fax_de = FaxDeService.new(settings['wsdl'], account)
 
 name_of_file_to_send = ARGV[0]
@@ -37,11 +37,14 @@ contents = File.open(path_to_file, 'rb') { |f| f.read }
 
 letter = FaxDeLetter.new(fax_de, name_of_file_to_send, contents)
 
+
 letter.generate_preview do |preview|
     File.open('result.tiff', 'wb') { |f| f.write(preview) }
     notifier.notify 'Generated preview file.'
 end
+
 exit if options[:dry_run]
+
 letter.send do
     notifier.notify "Sent ‘#{name_of_file_to_send}’."
 end
